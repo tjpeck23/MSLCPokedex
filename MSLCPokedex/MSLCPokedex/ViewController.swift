b0bfa8b07787a0683430c9cc1c9f34cd82536994
tjpeck23 <160659700+tjpeck23@users.noreply.github.com>
2024-09-17 22:16:33 -0500

    Adding initial files for Xcode project

---
+++ MSLCPokedex/MSLCPokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSLCPokedex
+//
+//  Created by Travis Peck on 9/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
